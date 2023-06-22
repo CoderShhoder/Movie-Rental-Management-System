a495ad746e7c3a39f78bdab3a275c325b2a7424e
CoderShhoder <harshh.raj16@gmail.com>
2023-06-22 12:32:19 +0530

    Added a Class in the Model Section and defined its properties ID, Name

---
+++ Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCapplication.Models
+{
+    public class Movie
+    {
+        //properties related to the movie class 
+        public int ID { get; set; }
+        public string Movies { get; set; }
+    }
+}
